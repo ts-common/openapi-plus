39b800d5b51a378cfc9669ddb94d51ae51a4c738
Sergey Shandar <sergey.shandar@gmail.com>
2018-07-23 14:08:23 -0700

    OpenAPI+

--- index.ts
+++ index.ts
@@ -1,8 +1,32 @@
+import * as openApi from "./openApi"
+import * as openApiPlus from "./openApiPlus"
+
 /**
  * The main function.
  */
 // tslint:disable-next-line:only-arrow-functions no-empty
-export function main(): void { }
+export function main(): void {
+    const oap: openApiPlus.Main = {
+        swagger: "2.0",
+        info: {
+            title: "Test",
+            version: "1.0"
+        },
+        paths: {
+            "somePath/sss": {
+                get: [
+                    {
+                        operationId: "a"
+                    },
+                    {
+                        operationId: "b"
+                    }
+                ]
+            }
+        }
+    }
+    const oa2: openApi.Main = oap
+}
 
 // tslint:disable-next-line:no-console
 console.log("test")
